352a25d01c7a021bab353417cf0a136623273483
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2021-02-01 18:35:46 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,44 +1,51 @@
 import React, { useState, useRef } from "react";
-import { View, ScrollView, Text, StyleSheet, TouchableOpacity} from "react-native";
-
-
+import { View, ScrollView, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export default () => {
-  return (
-    <ScrollView>
-      <Text style={StyleSheet.ano}>Ano da prova</Text>
-      <Text style={StyleSheet.pergunta}>Pergunta</Text>
-      <TouchableOpacity style={StyleSheet.a}><Text>A - </Text></TouchableOpacity>
-      <TouchableOpacity style={StyleSheet.b}><Text>B - </Text></TouchableOpacity>
-      <TouchableOpacity style={StyleSheet.c}><Text>C - </Text></TouchableOpacity>
-      <TouchableOpacity style={StyleSheet.d}><Text>D - </Text></TouchableOpacity>
-    </ScrollView>
-  );
+    return ( 
+      <ScrollView>
+        <View style = { StyleSheet.VPergunta } >
+          <Text style = { StyleSheet.ano } > Ano da prova </Text> 
+          <Text style = { StyleSheet.pergunta } > Pergunta </Text> 
+        </View> 
+        <View style = { StyleSheet.VResposta } >
+          <TouchableOpacity style = { StyleSheet.a } > < Text > A - </Text></TouchableOpacity >
+          <TouchableOpacity style = { StyleSheet.b } > < Text > B - </Text></TouchableOpacity >
+          <TouchableOpacity style = { StyleSheet.c } > < Text > C - </Text></TouchableOpacity >
+          <TouchableOpacity style = { StyleSheet.d } > < Text > D - </Text></TouchableOpacity>
+        </View> 
+      </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-  },
-  ano:{
-    marginTop: 35,
-    position: 'absolute',
-    color: '#9b111e',
-  },
-  pergunta:{
+    container: {
+        flex: 1,
+    },
+    VPergunta: {
+        marginTop: 35,
+    },
+    VResposta: {
 
-  },
-  a:{
+    },
+    ano: {
+        position: 'absolute',
+        color: '#9b111e',
+    },
+    pergunta: {
 
-  },
-  b:{
+    },
+    a: {
 
-  },
-  c:{
+    },
+    b: {
 
-  },
-  d:{
+    },
+    c: {
 
-  },
+    },
+    d: {
+
+    },
 
 });
