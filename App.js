cd1051026d060b1fd93350d3a009a8138e7db8aa
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2021-02-01 21:51:05 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,32 +1,71 @@
 import React, { useState, useRef } from "react";
-import { View, ScrollView, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, ScrollView, Text, Dimensions, StyleSheet, TouchableOpacity } from "react-native";
+
+const {width: screenWidth, height: screenHeight} = Dimensions.get('window');
 
 export default () => {
-    return ( 
+  const [selecionado, setSelecionado] = useState("");
+  
+  return ( 
       <ScrollView>
-        <View style = { StyleSheet.VPergunta } >
-          <Text style = { StyleSheet.ano } > Ano da prova </Text> 
-          <Text style = { StyleSheet.pergunta } > Pergunta </Text> 
+        <View style={{flex:1, height: screenHeight}}>
+        <View style = { styles.VPergunta } >
+          <Text style = { styles.ano } > Ano da prova </Text> 
+          <Text style = { styles.pergunta } > pergunta </Text> 
         </View> 
-        <View style = { StyleSheet.VResposta } >
+        <View style = { styles.VResposta } >
           
-          <TouchableOpacity style = { StyleSheet.a }> 
+          {selecionado == "a" &&
+          <TouchableOpacity style = { styles.sele } onPress={() => setSelecionado("a")}> 
             <Text> A - </Text>
           </TouchableOpacity>
-
-          <TouchableOpacity style = { StyleSheet.b }> 
+          } 
+          {selecionado != "a" &&
+          <TouchableOpacity style = { styles.sem } onPress={() => setSelecionado("a")}> 
+            <Text> A - </Text>
+          </TouchableOpacity>
+          } 
+          
+          {selecionado == "b" &&
+          <TouchableOpacity style = { styles.sele } onPress={() => setSelecionado("b")}> 
             <Text> B - </Text>
           </TouchableOpacity>
+          } 
+          {selecionado != "b" &&
+          <TouchableOpacity style = { styles.sem } onPress={() => setSelecionado("b")}> 
+            <Text> B - </Text>
+          </TouchableOpacity>
+          } 
 
-          <TouchableOpacity style = { StyleSheet.c }> 
+          {selecionado == "c" &&
+          <TouchableOpacity style = { styles.sele } onPress={() => setSelecionado("c")}> 
             <Text> C - </Text>
           </TouchableOpacity>
-          
-          <TouchableOpacity style = { StyleSheet.d }> 
+          } 
+          {selecionado != "c" &&
+          <TouchableOpacity style = { styles.sem } onPress={() => setSelecionado("c")}> 
+            <Text> C - </Text>
+          </TouchableOpacity>
+          } 
+
+          {selecionado == "d" &&
+          <TouchableOpacity style = { styles.sele } onPress={() => setSelecionado("d")}> 
             <Text> D - </Text>
           </TouchableOpacity>
+          } 
+          {selecionado != "d" &&
+          <TouchableOpacity style = { styles.sem } onPress={() => setSelecionado("d")}> 
+            <Text> D - </Text>
+          </TouchableOpacity>
+          } 
 
+
+          <TouchableOpacity style = { styles.enviar }> 
+            <Text> ENVIAR </Text>
+          </TouchableOpacity>
+          
         </View> 
+        </View>
       </ScrollView>
     );
 }
@@ -36,29 +75,45 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     VPergunta: {
-        marginTop: 35,
+        marginTop: 50,
+        justifyContent: 'center',
+        alignItems: 'center' 
     },
     VResposta: {
-
+		marginTop: 30,
+		justifyContent: 'center',
+        alignItems: 'center' 
     },
-    ano: {
-        position: 'absolute',
+    ano: {        
         color: '#9b111e',
     },
     pergunta: {
 
     },
-    a: {
-
+    sem: {
+      borderRadius: 6,
+      backgroundColor: '#d3d3d3',
+      width: '90%',
+      height: 30,
+      marginBottom: 20,
+	    justifyContent: 'center',
+    },   
+    sele: {
+	  	borderRadius: 6,
+      backgroundColor: '#90ee90',
+      width: '90%',
+      height: 30,
+      marginBottom: 20,
+	    justifyContent: 'center',
     },
-    b: {
-
-    },
-    c: {
-
-    },
-    d: {
-
-    },
+    enviar: {
+      borderRadius: 6,
+      backgroundColor: '#90ee90',
+      width: '40%',
+      height: 60,
+      marginTop: 20,
+	    justifyContent: 'center',
+	    alignItems: 'center'
+    }
 
 });
