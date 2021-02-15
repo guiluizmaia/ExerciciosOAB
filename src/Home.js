a139058958638bed2dbc73acfaab4d15a696dc58
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2021-02-14 22:31:21 -0300

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,13 @@
 import React, { useState, useRef } from "react";
 import { View, Image, Text, Dimensions, StyleSheet, TouchableOpacity } from "react-native";
 import { FontAwesome, FontAwesome5, MaterialCommunityIcons, MaterialIcons  } from '@expo/vector-icons';
-
+import { AdMobBanner } from 'expo-ads-admob';
 const {width: screenWidth, height: screenHeight} = Dimensions.get('window');
 
 export default ({ navigation}) => {
   return ( 
 	<View style={{flex: 1, backgroundColor: '#DCDCDC'}}>
+        
     <View style={{alignItems: 'center', justifyContent: 'center'}}>
         <Image source={require('./Imagens/logo.jpg')} style={styles.Logo}/>
         <Text style={{fontSize: 18, color: '#373737', textAlign: 'center', marginTop: '10%'}}> Contém questões dos anos 2019 e 2020 </Text>
